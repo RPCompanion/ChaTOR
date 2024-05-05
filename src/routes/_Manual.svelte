1800eb44f2012aae50387783c0b05937ea242ac5
Emma Davenport <davenport.physics@gmail.com>
2024-05-04 22:22:04 -0500

    Addition of clear chat button

--- src/routes/_Manual.svelte
+++ src/routes/_Manual.svelte
@@ -44,21 +44,30 @@
 
         show_modal = false;
         submit_post(messages);
-
+        
     }
 
     function on_no_confirmation() {
         show_modal = false;
     }
 
+    function clear_chat() {
+        messages = [""];
+    }
+
 </script>
 
 <div class="flex flex-col gap-2 w-full p-10">
     <div class="text-white text-center bg-slate-700 text-xl">Manual Formatting Mode</div>
+    <div class="relative h-6">
+        <button type="button" class="bg-slate-800 text-white rounded-sm shadow-sm w-32 absolute right-0" on:click={clear_chat}>Clear chat</button>
+    </div>
     {#each messages as message, idx}
         <div class="relative">
             <textarea class="w-full min-h-24 outline-none p-1 resize-none rounded-md border-2" class:border-yellow-400={message.length >= 200 && message.length <= 255} class:border-red-500={message.length > 255} bind:value={message}/>
-            <button class="absolute bg-slate-700 px-1 -right-2 -top-3 text-white" style="border-radius: 50%" on:click={() => { delete_message(idx); }}>X</button>
+            {#if idx != 0}
+                <button class="absolute bg-slate-700 px-1 -right-2 -top-3 text-white" style="border-radius: 50%" on:click={() => { delete_message(idx); }}>X</button>
+            {/if}
             <div class="absolute bottom-1 right-0">{message.length}/255</div>
         </div>
     {/each}
