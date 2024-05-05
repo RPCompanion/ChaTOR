8d6b8beda519844f91832d7fe0fe2063212354a0
Emma Davenport <davenport.physics@gmail.com>
2024-05-05 08:44:38 -0500

    Change: Stylized always on top toggle

--- src/routes/_FooterControls.svelte
+++ src/routes/_FooterControls.svelte
@@ -11,9 +11,9 @@
 
 </script>
 
-<div class="z-10 absolute bottom-0 left-0 w-full">
-    <label class="text-white text-xl">
-        <input type="checkbox" bind:checked={always_on_top}/>
+<div class="z-10 absolute bottom-2 left-2 flex flex-row gap-2">
+    <label class="text-white text-xl bg-slate-900 rounded-md shadow-lg p-1">
+        <input type="checkbox" bind:checked={always_on_top} class="ml-2 scale-150"/>
         Always on top
     </label>
 </div>
