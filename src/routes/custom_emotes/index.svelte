4e5fccf8ebef97e2271dc9def2909f8736fcaea3
Emma Davenport <davenport.physics@gmail.com>
2024-05-09 15:59:50 -0500

    Addition: svelte-dnd-action dependency for drag and drop

--- src/routes/custom_emotes/index.svelte
+++ src/routes/custom_emotes/index.svelte
@@ -1,6 +1,9 @@
 
 <script lang="ts">
 
+    import { flip } from "svelte/animate";
+    import { dndzone } from 'svelte-dnd-action';
+
     import { fade } from "svelte/transition";
     import XButton from "../../lib/buttons/XButton.svelte";
     import { custom_emotes, delete_custom_emote, update_custom_emote } from "../../lib/network/custom_emote";
@@ -8,30 +11,57 @@
 
     let show_modal = false;
     let show_x_button_idx: number | undefined = undefined;
+    let emotes: any = $custom_emotes;
+
+    set_emote_ids();
 
     function on_cancel_modal() {
         show_modal = false;
+    }
+
+    function handle_dnd_consider(e: any) {
+        emotes = e.detail.items;
+    }
+
+    function handle_dnd_finalize(e: any) {
+        emotes = e.detail.items;
+        $custom_emotes = emotes;
+    }
+
+    function set_emote_ids() {
+
+        emotes.forEach((emote: any) => {
+            emote.id = emote.custom_emote_id;
+        });
+
+    }
+
+    $: if ($custom_emotes.length != emotes.length) {
+
+        emotes = $custom_emotes;
+        set_emote_ids();
+
     }
 
 </script>
 
 <div class="h-12"></div>
 <div class="w-full relative">
-    <div class="flex flex-col gap-2 max-h-96 overflow-y-auto">
-        {#each $custom_emotes as emote, idx}
+    <section class="flex flex-col gap-2 max-h-96 overflow-y-auto" use:dndzone="{{items: emotes, flipDurationMs: 300}}" on:consider={handle_dnd_consider} on:finalize={handle_dnd_finalize}>
+        {#each emotes as emote (emote.id)}
             <!-- svelte-ignore a11y-no-static-element-interactions -->
             <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-            <div class="flex flex-row gap-2 px-2 relative" on:mouseenter={() => { show_x_button_idx = idx }} on:mouseleave={() => { show_x_button_idx = undefined }}>
+            <div animate:flip="{{ duration: 300 }}" class="flex flex-row gap-2 px-2 relative" on:mouseenter={() => { show_x_button_idx = emote.id }} on:mouseleave={() => { show_x_button_idx = undefined }}>
                 <input type="text" bind:value={emote.emote_name} class="w-1/2 text-xl outline-none border-slate-500 border-2 rounded-md px-1" on:focusout={() =>  { update_custom_emote(emote); }}/>
                 <input type="text" bind:value={emote.emote} class="w-1/2 text-xl outline-none border-slate-500 border-2 rounded-md px-1" on:focusout={() => { update_custom_emote(emote); }} />
-                {#if show_x_button_idx == idx}
+                {#if show_x_button_idx == emote.id}
                     <div class="absolute right-0 bottom-3" transition:fade|local={{ duration: 150 }}>
                         <XButton on:click={() => { delete_custom_emote(emote.custom_emote_id); }}/>
                     </div>
                 {/if}
             </div>
         {/each}
-    </div>
+    </section>
     <div class="h-6"></div>
     <div class="flex flex-row justify-center">
         <button class="bg-slate-700 text-white text-xl px-2 rounded-md w-32 hover:text-gray-500" on:click={() => { show_modal = true; }}>New Emote</button>
