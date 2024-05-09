6f34f5fa27eb8a186bdeeff94323e1021b238020
Emma Davenport <davenport.physics@gmail.com>
2024-05-09 12:48:37 -0500

    Addition: Standard x button

---
+++ src/lib/buttons/XButton.svelte
@@ -0,0 +1,8 @@
+
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+</script>
+
+
+<button class="bg-slate-700 px-2 text-white text-xl" style="border-radius: 50%" on:click={() => { dispatch("click"); }}>X</button>
