c0508143cc74d5d888aa8858092ba68c7ebc6af8
Emma Davenport <davenport.physics@gmail.com>
2024-05-09 14:20:54 -0500

    Change: Converted another button to XButton. Fade in when hovering over the element

--- src/routes/custom_emotes/index.svelte
+++ src/routes/custom_emotes/index.svelte
@@ -1,10 +1,13 @@
 
 <script lang="ts">
 
+    import { fade } from "svelte/transition";
+    import XButton from "../../lib/buttons/XButton.svelte";
     import { custom_emotes, delete_custom_emote, update_custom_emote } from "../../lib/network/custom_emote";
     import EmoteModal from "./_EmoteModal.svelte";
 
     let show_modal = false;
+    let show_x_button_idx: number | undefined = undefined;
 
     function on_cancel_modal() {
         show_modal = false;
@@ -15,11 +18,17 @@
 <div class="h-12"></div>
 <div class="w-full relative">
     <div class="flex flex-col gap-2">
-        {#each $custom_emotes as emote}
-            <div class="flex flex-row gap-2 px-2 relative">
+        {#each $custom_emotes as emote, idx}
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+            <div class="flex flex-row gap-2 px-2 relative" on:mouseenter={() => { show_x_button_idx = idx }} on:mouseleave={() => { show_x_button_idx = undefined }}>
                 <input type="text" bind:value={emote.emote_name} class="w-1/2 text-xl outline-none border-slate-500 border-2 rounded-md px-1" on:focusout={() =>  { update_custom_emote(emote); }}/>
                 <input type="text" bind:value={emote.emote} class="w-1/2 text-xl outline-none border-slate-500 border-2 rounded-md px-1" on:focusout={() => { update_custom_emote(emote); }} />
-                <button class=" bg-slate-600 text-white px-2 absolute right-0 bottom-3 rounded-full text-xl" on:click={() => { delete_custom_emote(emote.custom_emote_id); } }>X</button>
+                {#if show_x_button_idx == idx}
+                    <div class="absolute right-0 bottom-3" transition:fade|local={{ duration: 150 }}>
+                        <XButton on:click={() => { delete_custom_emote(emote.custom_emote_id); }}/>
+                    </div>
+                {/if}
             </div>
         {/each}
     </div>
--- src/routes/settings/_SettingsToggle.svelte
+++ src/routes/settings/_SettingsToggle.svelte
@@ -1,9 +1,6 @@
 
 <script lang="ts">
-
-    import { settings } from "../../lib/network/settings";
     export let checked: boolean;
-
 </script>
 
 <input type="checkbox" bind:checked={checked} class="scale-150" />
