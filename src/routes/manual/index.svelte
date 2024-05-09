a137dcc28841b13b49815248606dff4bbebc2d65
Emma Davenport <davenport.physics@gmail.com>
2024-05-09 12:16:30 -0500

    Change: Styled new button to be closer to clear chat.

--- src/routes/manual/index.svelte
+++ src/routes/manual/index.svelte
@@ -1,6 +1,8 @@
 
 <script lang="ts">
 
+    import { settings } from "../../lib/network/settings";
+    import { onMount } from "svelte";
     import { toast } from "@zerodevx/svelte-toast";
     import ConfirmationModal from "../../lib/ConfirmationModal.svelte";
     import Checkbox from "../../lib/Checkbox.svelte";
@@ -80,20 +82,48 @@
         automated_posting = event.detail;
     }
 
+    /*
+    onMount(() => {
+
+        function on_key_up(event: KeyboardEvent) {
+
+            if (!$settings.chat.enter_to_send) {
+                return;
+            }
+
+            if (event.key == "Enter" && !event.shiftKey) {
+                enable_confirmation_modal();
+            }
+
+        }
+
+        window.addEventListener("keyup", on_key_up);
+        return () => {
+            window.removeEventListener("keyup", on_key_up);
+        }
+
+    });
+    */
+
+    function on_submit(event: Event) {
+        event.preventDefault();
+        console.log("here")
+    }
+
 </script>
 
 <div class="flex flex-col gap-2 w-full p-10 relative">
     <div class="text-white text-center bg-slate-700 text-2xl">Manual Formatting Mode</div>
     <div class="relative h-6">
         <Checkbox on:checked={on_checked} checked={automated_posting} size="small">Automated posting</Checkbox>
-        <button type="button" class="bg-slate-800 text-white rounded-sm shadow-sm w-32 absolute right-0" on:click={clear_chat}>Clear chat</button>
+        <button type="button" class="bg-slate-800 text-white rounded-sm shadow-sm w-32 absolute right-0 hover:text-gray-300" on:click={clear_chat}>Clear chat</button>
     </div>
-    <div class="">
+    <form>
         {#each messages as message, idx}
             <div class="relative">
                 <textarea class="w-full min-h-24 outline-none p-1 resize-none rounded-md border-2" class:border-yellow-400={message.length >= 200 && message.length <= 255} class:border-red-500={message.length > 255} bind:value={message}/>
                 {#if idx != 0}
-                    <button class="absolute bg-slate-700 px-1 -right-2 -top-3 text-white" style="border-radius: 50%" on:click={() => { delete_message(idx); }}>X</button>
+                    <button class="absolute bg-slate-700 px-2 -right-2 -top-3 text-white text-xl" style="border-radius: 50%" on:click={() => { delete_message(idx); }}>X</button>
                 {/if}
                 <div class="absolute bottom-1 right-0">{message.length}/255</div>
                 {#if !automated_posting}
@@ -101,8 +131,11 @@
                 {/if}
             </div>
         {/each}
+    </form>
+    <div class="w-full h-6 flex flex-row-reverse">
+        <button type="button" class="bg-slate-800 text-white rounded-sm shadow-sm w-32 hover:text-gray-300" on:click={on_new_message}>New</button>
     </div>
-    <StandardMenuButton text="New" on:click={on_new_message}/>
+    <!--<StandardMenuButton text="New" on:click={on_new_message}/>-->
     {#if automated_posting}
         <StandardMenuButton text="Post all" on:click={enable_confirmation_modal}/>
     {/if}
