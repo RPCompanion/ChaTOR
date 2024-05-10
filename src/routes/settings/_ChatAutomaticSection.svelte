778c0cfb64b387845fc6a92cc5228f7335959b0d
Emma Davenport <davenport.physics@gmail.com>
2024-05-10 12:05:04 -0500

    Change: Hooked in new automatic settings

--- src/routes/settings/_ChatAutomaticSection.svelte
+++ src/routes/settings/_ChatAutomaticSection.svelte
@@ -10,4 +10,10 @@
     <Setting setting="Press enter to post">
         <SettingsToggle bind:checked={$settings.chat.enter_to_post}></SettingsToggle>
     </Setting>
+    <Setting setting="Remove starting pronouns in a post">
+        <SettingsToggle bind:checked={$settings.chat.remove_starting_pronouns}></SettingsToggle>
+    </Setting>
+    <Setting setting="Set the first character of a post to it's lowercase equivalent">
+        <SettingsToggle bind:checked={$settings.chat.starting_characters_are_lowercase}></SettingsToggle>
+    </Setting>
 </SettingSection>
