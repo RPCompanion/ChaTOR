8c5ee43fbcb1df18adb9ef3b3d5cb5dacb2bb9dc
Emma Davenport <davenport.physics@gmail.com>
2024-05-13 15:11:10 -0500

    Addition: Code to inject dll is now implemented

--- src-tauri/src/capture_injector.rs
+++ src-tauri/src/capture_injector.rs
@@ -1,11 +1,15 @@
 
 use serde::{Deserialize, Serialize};
-use std::sync::{atomic::{AtomicBool, Ordering}, Arc};
+use serde_json::{Deserializer, Value};
+
+use std::{io::Write, net::{TcpListener, TcpStream}, sync::{atomic::{AtomicBool, Ordering}, Arc, Mutex}, time::Duration};
 use std::thread;
 
 use dll_syringe::{process::OwnedProcess, Syringe};
 
-use crate::swtor_hook;
+use crate::{share::Message, swtor_hook};
+
+use self::message_container::MessageContainer;
 
 pub mod message_container;
 pub mod player_gui_state;
@@ -13,6 +17,8 @@ pub mod message_parser;
 
 lazy_static! {
     static ref INJECTED: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    static ref CONTINUE_LOGGING: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    static ref MESSAGE_CONTAINER: Arc<Mutex<MessageContainer>> = Arc::new(Mutex::new(MessageContainer::new()));
 }
 
 #[derive(Deserialize, Serialize)]
@@ -37,24 +43,68 @@ pub fn start_injecting_capture() -> Result<(), CaptureError> {
     if swtor_pid.is_none() {
         return Err(CaptureError::SwtorNotRunning);
     }
-
-    INJECTED.store(true, Ordering::Relaxed);
     let swtor_pid = swtor_pid.unwrap();
 
-    let target_process = OwnedProcess::from_pid(swtor_pid).unwrap();
-    let syringe = Syringe::for_process(target_process);
+    thread::spawn(move || {
 
+        INJECTED.store(true, Ordering::Relaxed);
+        CONTINUE_LOGGING.store(true, Ordering::Relaxed);
+        
+        let target_process = OwnedProcess::from_pid(swtor_pid).unwrap();
+        let syringe = Syringe::for_process(target_process);
+
+        let injected_payload = if cfg!(debug_assertions) {
+            syringe.inject("./target/debug/swtor_chat_capture.dll").unwrap()
+        } else {
+            syringe.inject("./swtor_chat_capture.dll").unwrap()
+        };
+
+        start_tcp_listener_loop();
+
+        thread::sleep(Duration::from_secs(5));
+        syringe.eject(injected_payload).unwrap();
+        INJECTED.store(false, Ordering::Relaxed);
+
+    });
     return Ok(());
 
 }
 
-fn start_tcp_listener() {
+fn start_tcp_listener_loop() {
 
-    
+    let msg_container = Arc::clone(&MESSAGE_CONTAINER);
+    let listener = TcpListener::bind("127.0.0.1:4392").unwrap();
+    let mut stream = listener.accept().unwrap().0;
+
+    while CONTINUE_LOGGING.load(Ordering::Relaxed) {
+
+        Deserializer::from_reader(&mut stream).into_iter::<Value>().for_each(|value| {
+
+            if let Ok(value) = value {
+
+                if let Ok(message) = serde_json::from_value(value) {
+
+                    msg_container.lock()
+                        .unwrap()
+                        .push(message)
+
+                }
+
+            }
+
+        });
+
+    }
+
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:4393") {
+        stream.write(b"stop").unwrap();
+    }
 
 }
 
 #[tauri::command]
 pub fn stop_injecting_capture() {
+
+    CONTINUE_LOGGING.store(false, Ordering::Relaxed);
 
 }
