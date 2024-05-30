e9723f30e74dc3ee451a83f96f8aa28e1b57997d
Emma Davenport <davenport.physics@gmail.com>
2024-05-30 10:56:14 -0500

    Change: Maximize button will now unmaximize when clicked again

--- src/routes/_NonDecoratedHeader.svelte
+++ src/routes/_NonDecoratedHeader.svelte
@@ -1,8 +1,10 @@
 
 <script lang="ts">
     import { appWindow } from "@tauri-apps/api/window";
-    import { X, Square, Minus  } from "phosphor-svelte";
+    import { X, Square, Minus, ArrowSquareDownLeft  } from "phosphor-svelte";
 
+
+    let is_maximized = false;
     function on_close() {
         appWindow.close();
     }
@@ -11,22 +13,37 @@
         appWindow.minimize();
     }
 
-    function on_maximize() {
-        appWindow.maximize();
+    async function on_maximize() {
+
+        if (await appWindow.isMaximized()) {
+
+            appWindow.unmaximize();
+            is_maximized = false;
+            return;
+            
+        }
+
+        await appWindow.maximize();
+        is_maximized = true;
+
     }
 
 </script>
 
 <body>
     <div data-tauri-drag-region class="h-8 bg-slate-700 class flex flex-row gap-1 fixed top-0 left-0 right-0 select-none justify-end ">
-        <button type="button" class="text-white hover:bg-slate-800" on:click={on_minimize}>
-            <Minus size={24}/>
+        <button type="button" class="text-white hover:bg-slate-800 px-1" on:click={on_minimize}>
+            <Minus size={22}/>
         </button>
-        <button type="button" class="text-white hover:bg-slate-800" on:click={on_maximize}>
-            <Square size={24}/>
+        <button type="button" class="text-white hover:bg-slate-800 px-1" on:click={on_maximize}>
+            {#if is_maximized}
+                <ArrowSquareDownLeft size={22}/>
+            {:else}
+                <Square size={22}/>
+            {/if}
         </button>
-        <button type="button" class="text-white hover:bg-slate-800" on:click={on_close}>
-            <X size={24}/>
+        <button type="button" class="text-white hover:bg-slate-800 px-1" on:click={on_close}>
+            <X size={22}/>
         </button>
     </div>
 </body>
