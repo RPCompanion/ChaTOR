19cced923e9cc1a63afb6236203525c5eaebd46e
Emma Davenport <davenport.physics@gmail.com>
2024-05-30 09:29:18 -0500

    Refactor: Removed redundant event dispatcher

--- src/routes/settings/_SettingsRangeSlider.svelte
+++ src/routes/settings/_SettingsRangeSlider.svelte
@@ -1,21 +1,14 @@
 
 <script lang="ts">
 
-    import { createEventDispatcher } from "svelte";
-
-    export let value: string;
+    export let value: number;
     export let min: number;
     export let max: number;
-
-    const dispatch = createEventDispatcher();
-    function on_change() {
-        dispatch("value_change", value);
-    }
 
 </script>
 
 <div class="">
-    <input type="range" bind:value={value} min={min.toString()} max={max.toString()} on:change={on_change} class="w-full cursor-pointer"/>
+    <input type="range" bind:value={value} min={min} max={max} class="w-full cursor-pointer"/>
     <div class="flex justify-between text-white text-xl">
         <span>{min}%</span>
         <span>{max}%</span>
