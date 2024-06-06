32ea2584e57b9a0499cd59e405ae8f7f79fb5c55
Emma Davenport <davenport.physics@gmail.com>
2024-06-06 14:46:48 -0500

    Change: Removed old copy database code (The only user that needed this has been migrated. Thanks Ag'a!)

--- src-tauri/src/dal/db.rs
+++ src-tauri/src/dal/db.rs
@@ -24,59 +24,7 @@ pub fn get_connection() -> Connection {
 
 pub fn init() {
 
-    check_to_copy_old_database();
-
     let conn = get_connection();
     conn.execute_batch(TABLES).expect("Error creating tables");
-
-}
-
-fn check_to_copy_old_database() {
-
-    if check_if_database_exists() {
-        return;
-    }
-
-    if !check_if_old_database_path_exists() {
-        return;
-    }
-
-    let em_dirs = dal::get_em_dirs();
-    let db_path = em_dirs.get_data_dir_path("blinky.db");
-    match fs::copy(get_old_database_path(), db_path) {
-        Ok(_) => {},
-        Err(err) => {
-            println!("Error copying old database: {}", err);
-        }
-    }
-
-}
-
-fn check_if_database_exists() -> bool {
-
-    let em_dirs = dal::get_em_dirs();
-    let db_path = em_dirs.get_data_dir_path("blinky.db");
-    Path::new(&db_path).exists()
-
-}
-
-fn check_if_old_database_path_exists() -> bool {
-
-    let old_db_path = get_old_database_path();
-    Path::new(&old_db_path).exists()
-
-}
-
-fn get_old_database_path() -> String {
-            
-    let em_dirs = dal::get_em_dirs();
-    let db_path = em_dirs.proj_dirs.data_dir().to_str().unwrap();
-
-    let old_db_path = Path::new(&db_path)
-        .parent().unwrap()
-        .parent().unwrap()
-        .join("SWTOR-Chat/data/blinky.db");
-
-    old_db_path.to_str().unwrap().to_string()
 
 }
