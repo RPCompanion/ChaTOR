d2509628a958a951f1434a30a0a4ec9baf1d1fee
Emma Davenport <davenport.physics@gmail.com>
2024-06-07 09:38:56 -0500

    Refactor: Added a _EmoteContainer component on the emotes page

---
+++ src/routes/custom_emotes/_EmoteContainer.svelte
@@ -0,0 +1,28 @@
+
+<script lang="ts">
+    import type { ICustomEmote } from "../../lib/network/custom_emote";
+    import Emote from "./_Emote.svelte";
+
+    export let emotes: ICustomEmote[];
+    export let favourite: boolean;
+
+    let items = get_relevant_items(emotes);
+    $: items  = get_relevant_items(emotes);
+
+
+    function get_relevant_items(emotes: ICustomEmote[]): ICustomEmote[] {
+
+        return emotes
+            .filter((e) => e.favourite == favourite)
+            .sort((a, b) => a.order_index - b.order_index);
+
+    }
+
+</script>
+
+<section class="flex flex-col gap-2">
+    {#each items as emote}
+        <Emote {emote} {favourite}/>
+    {/each}
+</section>
+
--- src/routes/custom_emotes/index.svelte
+++ src/routes/custom_emotes/index.svelte
@@ -1,10 +1,7 @@
 
 <script lang="ts">
 
-    import { afterUpdate } from "svelte";
-    import { dndzone } from 'svelte-dnd-action';
-
-    import Emote from "./_Emote.svelte";
+    import EmoteContainer from "./_EmoteContainer.svelte";
     import { custom_emotes } from "../../lib/network/custom_emote";
     import EmoteModal from "./_EmoteModal.svelte";
 
@@ -12,7 +9,6 @@
     let emotes: any = $custom_emotes;
 
     let scroll_bottom: boolean = false;
-    let emote_section: HTMLElement;
 
     set_emote_ids();
 
@@ -22,15 +18,6 @@
 
     function on_save_modal() {
         show_modal = false;
-    }
-
-    function handle_dnd_consider(e: any) {
-        emotes = e.detail.items;
-    }
-
-    function handle_dnd_finalize(e: any) {
-        emotes = e.detail.items;
-        $custom_emotes = emotes;
     }
 
     function set_emote_ids() {
@@ -52,42 +39,25 @@
 
     }
 
-    afterUpdate(() => {
-
-        if (scroll_bottom) {
-            const last_emote = emote_section.lastElementChild;
-            if (last_emote) {
-                last_emote.scrollIntoView({ behavior: "smooth" });
-            }
-            scroll_bottom = false;
-        }
-
-    });
-
 </script>
 
 <div class="h-12"></div>
 <div class="w-full relative">
-    <section bind:this={emote_section} class="flex flex-col gap-2 max-h-96 overflow-y-auto">
-
+    <div>
         <div class="h-2"></div>
         {#if $custom_emotes.filter((e) => e.favourite).length > 0}
             <p class="text-white text-2xl text-center">Favourite Emotes</p>
+            <div class="h-2"></div>
         {/if}
-        {#each $custom_emotes.filter((e) => e.favourite).sort((a, b) => a.order_index - b.order_index) as emote}
-            <Emote {emote} favourite={true}/>
-        {/each}
+        <EmoteContainer emotes={$custom_emotes} favourite={true}/>
 
-        
         {#if $custom_emotes.filter((e) => !e.favourite).length > 0 && $custom_emotes.filter((e) => e.favourite).length > 0}
             <div class="h-1"></div>
             <p class="text-white text-2xl text-center">Emotes</p>
+            <div class="h-2"></div>
         {/if}
-        {#each $custom_emotes.filter((e) => !e.favourite).sort((a, b) => a.order_index - b.order_index) as emote}
-            <Emote {emote} favourite={false}/>
-        {/each}
-
-    </section>
+        <EmoteContainer emotes={$custom_emotes} favourite={false}/>
+    </div>
     <div class="h-6"></div>
     <div class="flex flex-row justify-center">
         <button class="bg-slate-700 text-white text-xl px-2 rounded-md w-32 hover:text-gray-500" on:click={() => { show_modal = true; }}>New Emote</button>
