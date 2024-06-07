c2ab753e5cbbde6b4716b6de1e0b7bb6a07dceb6
Emma Davenport <davenport.physics@gmail.com>
2024-06-07 11:08:56 -0500

    Refactor: Introduced a IDNDCustomEmote interface. Changed location of get_relevant_items

---
+++ src/lib/custom_emote_utils.ts
@@ -0,0 +1,14 @@
+import type { ICustomEmote } from "./network/custom_emote";
+
+export interface IDNDCustomEmote extends ICustomEmote {
+    id: number;
+}
+
+export function get_relevant_emotes_from_list(emotes: ICustomEmote[], favourite: boolean): IDNDCustomEmote[] {
+
+    return emotes
+        .filter((e) => e.favourite == favourite)
+        .sort((a, b) => a.order_index - b.order_index)
+        .map((emote, index) => ({ ...emote, id: index }));
+
+}
--- src/routes/custom_emotes/_EmoteContainer.svelte
+++ src/routes/custom_emotes/_EmoteContainer.svelte
@@ -3,33 +3,21 @@
     import { dndzone, type DndEvent } from "svelte-dnd-action";
     import { update_custom_emotes_batch, type ICustomEmote } from "../../lib/network/custom_emote";
     import Emote from "./_Emote.svelte";
+    import { get_relevant_emotes_from_list, type IDNDCustomEmote } from "../../lib/custom_emote_utils";
 
     export let emotes: ICustomEmote[];
     export let favourite: boolean;
 
-    let items: DNDItem[] = get_relevant_items(emotes);
-    $: items  = get_relevant_items(emotes);
+    let items: IDNDCustomEmote[] = get_relevant_emotes_from_list(emotes, favourite);
+    $: items = get_relevant_emotes_from_list(emotes, favourite);
 
-    interface DNDItem extends ICustomEmote {
-        id: number;
-    }
-
-    function get_relevant_items(emotes: ICustomEmote[]): DNDItem[] {
-
-        return emotes
-            .filter((e) => e.favourite == favourite)
-            .sort((a, b) => a.order_index - b.order_index)
-            .map((emote, index) => ({ ...emote, id: index }));
-
-    }
-
-    function on_consider(e: CustomEvent<DndEvent<DNDItem>>) {
+    function on_consider(e: CustomEvent<DndEvent<IDNDCustomEmote>>) {
 
         items = e.detail.items;
 
     }
 
-    function on_finalize(e: CustomEvent<DndEvent<DNDItem>>) {
+    function on_finalize(e: CustomEvent<DndEvent<IDNDCustomEmote>>) {
 
         items = e.detail.items;
         items.forEach((emote, index) => {
