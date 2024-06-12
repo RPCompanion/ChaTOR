b61ca0db38dfbf2a35eb552125f2ad4c17791246
Emma Davenport <davenport.physics@gmail.com>
2024-06-12 16:29:24 -0500

    Change: Handle blauncher copying programmatically, rather than letting tauri do the work

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,8 +2,11 @@ fn main() {
 
     let mut windows = tauri_build::WindowsAttributes::new();
     if cfg!(debug_assertions) {
+
         tauri_build::build()
+
     } else {
+
         windows = windows.app_manifest(r#"
             <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
                 <dependency>
@@ -29,6 +32,27 @@ fn main() {
         "#);
         let attrs = tauri_build::Attributes::new().windows_attributes(windows);
         tauri_build::try_build(attrs).expect("error while building tauri application");
+        finalize_release();
+        
     }
     
+}
+
+fn finalize_release() {
+    
+    copy_blauncher();
+    
+}
+
+/*
+    blauncher is only needed for builds that are released on itch.io, and not needed for builds that are
+    built through github actions.
+*/
+fn copy_blauncher() {
+
+    match std::fs::copy("./misc/blauncher.exe", "./target/release/misc/blauncher.exe") {
+        Ok(_) => println!("blauncher.exe copied successfully"),
+        Err(e) => println!("Error while copying blauncher.exe: {}", e)
+    }
+
 }
