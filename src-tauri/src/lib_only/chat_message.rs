da8bda553cef205968e2fa9eb6352a45e355879e
Emma Davenport <davenport.physics@gmail.com>
2024-06-23 19:37:41 -0500

    Change: Reading chat messages is now extra safe

--- src-tauri/src/lib_only/chat_message.rs
+++ src-tauri/src/lib_only/chat_message.rs
@@ -1,5 +1,4 @@
 
-use std::ffi::CStr;
 use std::mem;
 use retour::static_detour;
 
@@ -20,11 +19,11 @@ pub fn begin_detour(base_address: isize) {
         let target: extern "C" fn(*mut u64, *const *const i8, *const *const i8, i32, *const *const i8) -> i64 = mem::transmute(base_address + CHAT_RELATIVE_ADDRESS);
         match ChatHook.initialize(target, receive_chat_message_detour) {
             Ok(_) => {
-                submit_message(CaptureMessage::Info("Detour initialized".to_string()));
+                submit_message(CaptureMessage::Info("Chat Message detour initialized".to_string()));
                 ChatHook.enable().unwrap();
             },
             Err(_) => {
-                submit_message(CaptureMessage::CaptureError("Failed to initialize detour".to_string()));
+                submit_message(CaptureMessage::CaptureError("Failed to initialize chat message detour".to_string()));
             }
         }
 
@@ -34,16 +33,14 @@ pub fn begin_detour(base_address: isize) {
 
 pub fn receive_chat_message_detour(param_1: *mut u64, from: *const *const i8, to: *const *const i8, channel_id: i32, chat_message: *const *const i8) -> i64 {
 
-    unsafe {
-
-        let t_from         = CStr::from_ptr(*from).to_str().unwrap();
-        let t_to           = CStr::from_ptr(*to).to_str().unwrap();
-        let t_chat_message = CStr::from_ptr(*chat_message).to_str().unwrap();
-
-        submit_message(CaptureMessage::Chat(RawSwtorMessage::new(channel_id, t_from.to_string(), t_to.to_string(), t_chat_message.to_string())));
-        return ChatHook.call(param_1, from, to, channel_id, chat_message);
-
+    match RawSwtorMessage::from_raw_ptrs(channel_id, from, to, chat_message) {
+        Ok(message) => {
+            submit_message(CaptureMessage::Chat(message));
+        },
+        Err(_) => {}
     }
+
+    return ChatHook.call(param_1, from, to, channel_id, chat_message);
 
 }
 
--- src-tauri/src/share/raw_swtor_message.rs
+++ src-tauri/src/share/raw_swtor_message.rs
@@ -1,4 +1,6 @@
 
+use std::ffi::CStr;
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -25,8 +27,31 @@ impl RawSwtorMessage {
 
     }
 
+    pub fn from_raw_ptrs(channel_id: i32, from: *const *const i8, to: *const *const i8, chat_message: *const *const i8) -> Result<RawSwtorMessage, &'static str> {
+
+        let converter = |ptr: *const *const i8| -> Result<String, &'static str> {
+
+            unsafe {
+                Ok(CStr::from_ptr(*ptr)
+                    .to_str()
+                    .map_err(|_| "Failed to convert")?
+                    .to_string())
+            }
+
+        };
+
+        let t_from         = converter(from)?;
+        let t_to           = converter(to)?;
+        let t_chat_message = converter(chat_message)?;
+
+        Ok(RawSwtorMessage::new(channel_id, t_from, t_to, t_chat_message))
+
+    }
+
     pub fn as_json_str(&self) -> String {    
+
         serde_json::to_string(self).unwrap()
+
     }
 
 }
