141a08d3303cdd056cefacd0d03d480cb3ae9350
Emma Davenport <davenport.physics@gmail.com>
2024-06-25 07:58:45 -0500

    Refactor: Placed constants outside of function. Introduced WinToUnix trait in logging

--- src-tauri/src/logging.rs
+++ src-tauri/src/logging.rs
@@ -7,6 +7,9 @@ use chrono::prelude::*;
 use tracing_subscriber::{self, fmt, prelude::*};
 use tracing_appender::{self, non_blocking::WorkerGuard};
 
+const WINDOWS_TICK: u64      = 10000000;
+const SEC_TO_UNIX_EPOCH: u64 = 11644473600;
+
 pub fn init() -> WorkerGuard {
 
     let path = Path::new("./logs");
@@ -52,7 +55,7 @@ fn delete_stale_files() {
         let metadata = fs::metadata(&file)
             .expect("error while reading file metadata");
 
-        let date_time = DateTime::from_timestamp(windows_epoch_to_unix_epoch(metadata.last_write_time()), 0).unwrap();
+        let date_time = DateTime::from_timestamp(metadata.last_write_unix_epoch(), 0).unwrap();
         let current_time = Utc::now();
         if (current_time - date_time).num_days() >= 7 {
 
@@ -65,11 +68,14 @@ fn delete_stale_files() {
 
 }
 
-fn windows_epoch_to_unix_epoch(windows_epoch: u64) -> i64 {
-    
-    const WINDOWS_TICK: u64 = 10000000;
-    const SEC_TO_UNIX_EPOCH: u64 = 11644473600;
+trait WinToUnix {
+    fn last_write_unix_epoch(&self) -> i64;
+}
 
-    ((windows_epoch / WINDOWS_TICK) - SEC_TO_UNIX_EPOCH) as i64
+impl<T: MetadataExt> WinToUnix for T {
+
+    fn last_write_unix_epoch(&self) -> i64 {
+        ((self.last_write_time() / WINDOWS_TICK) - SEC_TO_UNIX_EPOCH) as i64
+    }
 
 }
