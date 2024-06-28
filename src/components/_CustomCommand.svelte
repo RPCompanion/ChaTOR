ab6048e975b0006433a71ba11f28e91125eaa295
Emma Davenport <davenport.physics@gmail.com>
2024-06-28 14:08:55 -0500

    ChangE: Support for setting CustomCommand with new ChannelDispatcher functionality

--- src/components/_CustomCommand.svelte
+++ src/components/_CustomCommand.svelte
@@ -3,27 +3,74 @@
     
     import { ESwtorChannel } from "../lib/network/swtor_channel";
     import { active_chat_tab_index } from "../lib/chat_log_window/chat_log_window_store";
-    import { settings } from "../lib/network/settings";
+    import { custom_channels } from "../lib/network/custom_channels";
+    import { settings, type ChannelDispatcher } from "../lib/network/settings";
 
     let channels: string[] = Object.keys(ESwtorChannel)
         .filter((key) => isNaN(Number(key)))
         .filter((key) => key != "WHISPER" && key != "CUSTOM_CHANNEL");
 
-    function get_value(channel: string): number {
+    let value: number | string | undefined = undefined
+    $: $active_chat_tab_index, set_value();
+    $: value, set_settings();
 
+    function get_channel_number(channel: string): number {
         return ESwtorChannel[channel as keyof typeof ESwtorChannel]
+    }
+
+    function set_value() {
+
+        let default_channel = $settings.chat_log.window.chat_tabs[$active_chat_tab_index].default_channel;
+
+        if (default_channel == undefined) {
+
+            value = undefined;
+            return;
+
+        }
+
+        if ("RegularDispatch" in default_channel) {
+            value = default_channel.RegularDispatch
+        } else {
+            value = default_channel.CustomDispatch
+        }
 
     }
+
+    function set_settings() {
+
+        if (value == undefined) {
+            return;
+        }
+
+        if (typeof value == "number") {
+            $settings.chat_log.window.chat_tabs[$active_chat_tab_index].default_channel = { RegularDispatch: value };
+        } else {
+            $settings.chat_log.window.chat_tabs[$active_chat_tab_index].default_channel = { CustomDispatch: value };
+        }
+
+    }
+
+    function on_change() {
+        console.log(value, typeof value);
+    }
+
+    set_value();
 
 </script>
 
 <div>
     <select 
-        bind:value={$settings.chat_log.window.chat_tabs[$active_chat_tab_index].default_channel}
+        bind:value={value}
+        on:change={on_change}
         class="rounded-md outline-none border-none bg-slate-600 text-white px-1 select-none">
         <option value={undefined}>none</option>
         {#each channels as channel}
-            <option value={get_value(channel)}>{channel.toLowerCase().replace("_", " ")}</option>
+            <option value={get_channel_number(channel)}>{channel.toLowerCase().replace("_", " ")}</option>
+        {/each}
+        <hr/>
+        {#each $custom_channels as channel}
+            <option value={channel.channel_name}>{channel.channel_name.toLowerCase()}</option>
         {/each}
     </select>
 </div>
