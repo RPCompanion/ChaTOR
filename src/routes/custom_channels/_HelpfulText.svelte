ccd94ed1909ca45117f92c9ed034b5e1dc696127
Emma Davenport <davenport.physics@gmail.com>
2024-07-02 12:42:36 -0500

    Addition: Helpful text on adding a custom channel

---
+++ src/routes/custom_channels/_HelpfulText.svelte
@@ -0,0 +1,19 @@
+
+<script lang="ts">
+    import { open_link } from "../../lib/network";
+
+    function show_forum_link() {
+        open_link("https://forums.swtor.com/topic/609549-how-to-use-custom-chat-channels/");
+    }
+
+</script>
+
+<div class="text-center text-lg">
+    <p class="text-white">This page allows you to specify any custom channels you have joined in game.</p>
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <span class="text-white">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        For more information on how custom channels work, see <span class="text-red-400 cursor-pointer" on:click={show_forum_link}>here</span>
+    </span>
+    <p class="text-white text-sm">The channel name should match the in game channel that you joined.</p>
+</div>
--- src/routes/custom_channels/index.svelte
+++ src/routes/custom_channels/index.svelte
@@ -1,8 +1,10 @@
 
 <script lang="ts">
+    import HelpfulText from "./_HelpfulText.svelte";
     import AddNew from "./_AddNew.svelte";
     import Existing from "./_Existing.svelte";
 </script>
 
+<HelpfulText/>
 <Existing/>
 <AddNew/>
