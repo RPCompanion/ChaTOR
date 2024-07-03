e1b82c6d0b866d78a06c7303e8fd1082099f7eac
Emma Davenport <davenport.physics@gmail.com>
2024-07-03 10:33:05 -0500

    Addition: functions in HyperlinkParser that will return HyperlinkTypes

--- src/lib/hyperlink_parser.ts
+++ src/lib/hyperlink_parser.ts
@@ -42,16 +42,110 @@ class HyperlinkParser {
         let type = Number(this.read_int());
 
         switch (type) {
-            case 2: break;
-            case 4: break;
-            case 5: break;
-            case 6: break;
-            case 8: break;
-            case 9: break;
-            default: break;
+            case 2: return Ok(this.get_item());
+            case 4: return Ok(this.get_quest());
+            case 5: return Ok(this.get_url());
+            case 6: return Ok(this.get_achievement());
+            case 8: return Ok(this.get_guild());
+            case 9: return Ok(this.get_pvp());
+            default: return Ok(this.get_unknown());
         }
 
-        return Err("Unable to parse hyperlink type.");
+    }
+
+    private get_item(): HyperlinkItem {
+
+        let get_craftable = (): ItemCraftable | undefined => {
+
+            if (this.text.substring(this.pos, this.pos + 3) == "OAA") {
+
+                return {
+                    schematic: this.read_id(),
+                    modifier: this.read_int(),
+                    unknown: this.read_int()
+                }
+
+            } else if (this.text.substring(this.pos, this.pos + 11) == "AAAAAAAAAAA") {
+
+                return {
+                    schematic: this.read_id()
+                }
+
+            } else {
+
+                return undefined;
+
+            }
+
+        }
+
+        return {
+            type: "item",
+            id: this.read_id(),
+            const_false: this.read_boolean() ? true : undefined,
+            craftable: get_craftable(),
+            modifier: this.text.substring(this.pos, this.pos + 1) === 'C' ? this.read_int() : undefined,
+            augmented: this.read_int(),
+            mods: [...Array(Number(this.read_int()))].map(() => ({ id: this.read_id(), modifier: this.read_int(), unknown: this.read_int() })),
+            final33: this.read_int(),
+            remainder: this.get_remainder()
+        }
+
+    }
+
+    private get_quest(): HyperLinkQuest {
+
+        return {
+            type: "quest",
+            id: this.read_id(),
+            const1: this.read_int(),
+            questStep: this.read_int(),
+            remainder: this.get_remainder()
+        }
+
+    }
+
+    private get_url(): HyperlinkURL {
+        
+        return { type: "url", index: this.read_int(), remainder: this.get_remainder()};
+
+    }
+
+    private get_achievement(): HyperlinkAchievement {
+        
+        return {
+            type: "achievement",
+            id: this.read_id(),
+            character: this.read_string(),
+            completed: this.read_boolean() && this.read_date(),
+            objectives: this.read_final_int_list(),
+            remainder: this.get_remainder()
+        }
+
+    }
+
+    private get_guild(): HyperlinkGuild {
+        
+        return { type: "guild", id: this.read_id(), name: this.read_string(), remainder: this.get_remainder() };
+
+    }
+
+    private get_pvp(): HyperlinkPVP {
+
+        return { 
+            type: "pvp",
+            char_name: this.read_string(),
+            char_id: this.read_id(),
+            discipline: this.read_id(),
+            queue: ["arena", "warzone"][Number(this.read_int())] ?? "unknown",
+            remainder: this.get_remainder()
+        }
+
+    }
+
+    private get_unknown(): HyperlinkUnkown {
+
+        return { type: "unknown", remainder: this.get_remainder() };
 
     }
 
@@ -135,36 +229,56 @@ class HyperlinkParser {
 
 }
 
+export interface ItemCraftable {
+    schematic: bigint;
+    modifier?: bigint;
+    unknown?: bigint;
+}
+
+export interface ItemMod {
+    id: bigint;
+    modifier: bigint;
+    unknown: bigint;
+}
+
 export interface HyperlinkItem {
     type: "item";
-    id: string;
+    id: bigint;
+    augmented: bigint;
+    const_false?: true;
+    craftable?: ItemCraftable;
+    modifier?: bigint;
+    mods: ItemMod[];
+    final33: bigint;
+    remainder?: string;
 }
 
 export interface HyperLinkQuest {
     type: "quest";
-    id: string;
-    const1: number;
-    questStep: string;
+    id: bigint;
+    const1: bigint;
+    questStep: bigint;
     remainder?: string;
 }
 
 export interface HyperlinkURL {
     type: "url";
-    index: string;
+    index: bigint;
+    remainder?: string;
 }
 
 export interface HyperlinkAchievement {
     type: "achievement";
-    id: string;
+    id: bigint;
     character: string;
-    completed: boolean;
-    objectives: string[];
+    completed: false | Date;
+    objectives: bigint[];
     remainder?: string;
 }
 
 export interface HyperlinkGuild {
     type: "guild";
-    id: string;
+    id: bigint;
     name: string;
     remainder?: string;
 }
@@ -172,8 +286,8 @@ export interface HyperlinkGuild {
 export interface HyperlinkPVP {
     type: "pvp";
     char_name: string;
-    char_id: string;
-    discipline: string;
+    char_id: bigint;
+    discipline: bigint;
     queue: string;
     remainder?: string;
 }
