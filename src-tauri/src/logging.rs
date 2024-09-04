a9ad2f873e8828c7da251c6534230622dd2cb3ab
Emma Davenport <davenport.physics@gmail.com>
2024-09-04 15:25:45 -0500

    Bugfix: Allow color parsing to fail (replicated, not merged)

--- src-tauri/src/logging.rs
+++ src-tauri/src/logging.rs
@@ -4,7 +4,8 @@ use std::path::Path;
 use std::os::windows::fs::MetadataExt;
 
 use chrono::prelude::*;
-use tracing_subscriber::{self, fmt, prelude::*};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{self, fmt, prelude::*, EnvFilter};
 use tracing_appender::{self, non_blocking::WorkerGuard};
 
 const WINDOWS_TICK: u64      = 10000000;
@@ -18,11 +19,26 @@ pub fn init() -> WorkerGuard {
         fs::create_dir("./logs")
             .expect("error while creating logs directory");
 
+    } else {
+
+        delete_stale_files();
+
     }
-    delete_stale_files();
 
     let file_appender = tracing_appender::rolling::daily("./logs", "chator.log");
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+    let env_filter = if cfg!(debug_assertions) {
+
+        EnvFilter::builder()
+            .with_default_directive(LevelFilter::TRACE.into())
+            .from_env_lossy()
+
+    } else {
+
+        EnvFilter::from("error")
+        
+    };
 
     tracing_subscriber::registry()
         .with(fmt::layer()
@@ -30,6 +46,7 @@ pub fn init() -> WorkerGuard {
             .with_level(true)
             .with_ansi(false)
         )
+        .with(env_filter)
         .init();
 
     guard
