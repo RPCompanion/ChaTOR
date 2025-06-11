e5d81437b5971a5b7fc9b1e509c336718a91fc86
Emma Davenport <davenport.physics@gmail.com>
2025-06-11 19:10:13 -0400

    Updated to version 0.1.31 (7.7c)

--- src-tauri/src/capture_injector.rs
+++ src-tauri/src/capture_injector.rs
@@ -19,7 +19,7 @@ mod syringe_container;
 
 use self::message_container::SwtorMessageContainer;
 
-const SUPPORTED_SWTOR_CHECKSUM: [u8; 32] = sha256_to_array!("ADCE3C524D86209F838BA898794F8E14FC49617C0374FCCA1383B32085691097");
+const SUPPORTED_SWTOR_CHECKSUM: [u8; 32] = sha256_to_array!("B71B6CF2548D6F65E0FC8227B207141DD228258BFB94F77794DBACFC415D8551");
 
 static MESSAGE_CONTAINER: LazyLock<Mutex<SwtorMessageContainer>> = LazyLock::new(|| Mutex::new(SwtorMessageContainer::new()));
 
